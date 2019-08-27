cab805ae87502385a32f7b46863c58d3d1b1af91
Damian Gurgul <damian.gurgul91@gmail.com>
2019-08-27 21:52:30 +0200

    lotto ver 1.01

--- lotto/js/main.js
+++ lotto/js/main.js
@@ -1,24 +1,33 @@
 'use strict'
 let ballsLotto = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49]
 let result = [];
+const globalBody = document.body;
 const ballsAll =() => {
-
     for( let i=0; i<ballsLotto.length; i++){
         const header = document.querySelector("header");
         let span = document.createElement("span");
-    
-    span.textContent=ballsLotto[i];
-    span.setAttribute("class", "pileczka")
-    
-    header.appendChild(span);
-    
+        for(let j=0; j<result.length; j++){
+            if(ballsLotto[i] === result[j]){
+                console.log(ballsLotto[i]);
+                span.setAttribute("class", "pileczka2");
+            }
+    else {
+          span.setAttribute("class", "pileczka");
+        }
+        span.textContent=ballsLotto[i];
+        header.appendChild(span); 
     }
-
+    
 }
+}
+       
+
 const randomize = () =>{
 
-    if (result.length === 6) return;
-
+    if (result.length === 6) {
+        ballsAll();
+        return;
+    }
     let number = Math.floor(Math.random() * 49 +1);
     for(let i=0;i<result.length; i++){
         if (number === result[i] || number === (result[i] + 1) || number === (result[i] - 1))
@@ -28,20 +37,17 @@ const randomize = () =>{
     let div = document.createElement("div");
     
     div.textContent=number;
-    document.body.appendChild(div);
+    div.setAttribute("class", "randomBall");
+    globalBody.appendChild(div);
     result.push(number);
-    for( let i=0; i<ballsLotto.length; i++){
-        if( number === ballsLotto[i]){
-            console.log(number);
-        }
-    }
+    
 }
 let count=0;
 const rem = () => {
     if(count===5) {result=[];
     }
     let deleteElement = document.querySelector("div")
-    document.body.removeChild(deleteElement); 
+    globalBody.removeChild(deleteElement); 
     if (count < 5)
     count++;
     else 
@@ -53,10 +59,10 @@ const remAll = ()=> {
     result=[];
     for(let i=0; i<6; i++){
         let deleteElement = document.querySelector("div")
-        document.body.removeChild(deleteElement); 
+        globalBody.removeChild(deleteElement); 
     }
 }
-ballsAll();
+
 const button = document.querySelector("button");
 button.addEventListener("click", randomize);
 const buttonRed = document.querySelector(".btn-r");

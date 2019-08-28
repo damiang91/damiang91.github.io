c4b7f0403cb71c8ffbfc2163b156d712d193d409
Damian Gurgul <damian.gurgul91@gmail.com>
2019-08-28 21:21:41 +0200

    lotto ver 1.02

--- lotto/js/main.js
+++ lotto/js/main.js
@@ -1,14 +1,38 @@
 'use strict'
 let ballsLotto = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49]
 let result = [];
+let countBalls =0;
 const globalBody = document.body;
 const ballsAll =() => {
+    if(countBalls === 0){
+    for( let i=0; i<ballsLotto.length; i++){
+        const header = document.querySelector(".header");
+        let span = document.createElement("span");
+        span.setAttribute("class", "pileczka");
+        span.textContent=ballsLotto[i];
+        header.appendChild(span); 
+        countBalls=1;
+    }
+    }
+    
+}
+const ballsRemAll = ()=> {
+    if(countBalls === 1){
+    for(let i=0; i<ballsLotto.length; i++){
+        const header = document.querySelector(".header");
+        let deleteBalls = header.querySelector("span");
+        header.removeChild(deleteBalls); 
+        countBalls=0;
+    }
+}
+}
+
+const ballsEnd =() => {
     for( let i=0; i<ballsLotto.length; i++){
         const header = document.querySelector("header");
         let span = document.createElement("span");
         for(let j=0; j<result.length; j++){
             if(ballsLotto[i] === result[j]){
-                console.log(ballsLotto[i]);
                 span.setAttribute("class", "pileczka2");
             }
     else {
@@ -17,7 +41,7 @@ const ballsAll =() => {
         span.textContent=ballsLotto[i];
         header.appendChild(span); 
     }
-    
+    countBalls=0;
 }
 }
        
@@ -25,7 +49,8 @@ const ballsAll =() => {
 const randomize = () =>{
 
     if (result.length === 6) {
-        ballsAll();
+        ballsRemAll();
+        ballsEnd();
         return;
     }
     let number = Math.floor(Math.random() * 49 +1);
@@ -46,7 +71,7 @@ let count=0;
 const rem = () => {
     if(count===5) {result=[];
     }
-    let deleteElement = document.querySelector("div")
+    let deleteElement = document.querySelector(".randomBall")
     globalBody.removeChild(deleteElement); 
     if (count < 5)
     count++;
@@ -58,15 +83,19 @@ const rem = () => {
 const remAll = ()=> {
     result=[];
     for(let i=0; i<6; i++){
-        let deleteElement = document.querySelector("div")
+        let deleteElement = document.querySelector(".randomBall")
         globalBody.removeChild(deleteElement); 
     }
 }
-
-const button = document.querySelector("button");
+ballsAll();
+const button = document.querySelector(".btn");
 button.addEventListener("click", randomize);
 const buttonRed = document.querySelector(".btn-r");
 buttonRed.addEventListener("click", rem );
 const buttonAll = document.querySelector(".btn-all");
 buttonAll.addEventListener("click", remAll)
+const buttonShow = document.querySelector(".btn-show");
+buttonShow.addEventListener("click", ballsAll);
+const buttonHide = document.querySelector(".btn-hide");
+buttonHide.addEventListener("click", ballsRemAll);
 
